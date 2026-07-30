1c95db92369453e6449e20b98140662cb8a10944
iamkendru <078bei014@ioepc.edu.np>
2026-07-30 09:42:28 +0545

    update

--- app/page.tsx
+++ app/page.tsx
@@ -4,10 +4,10 @@ export default function Page() {
   return (
     <section>
       <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
-        My Portfolio
+        Kendra (kendru) Pokhrel
       </h1>
       <p className="mb-4">
-        {`I'm Kendra POkhrel, a technology enthusiast and an Electronics and Communication Engineering enthusiast.
+        {`I'm Kendra Pokhrel, a technology enthusiast and an Electronics and Communication Engineering enthusiast.
         You can call me kendru. I've been learning web development components such as react, typescript and most part of frontend development.
         I also have experience in hardware, IoT, networking and partly homelabbing, as well as I have spent my fair share of time modifying embedded hardwares.
         I am a self made photographer and also have experience in literature.`}
