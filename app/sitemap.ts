6b3b69afb4ae8f9a33433e66c07ecf07dd9a851d
iamkendru <078bei014@ioepc.edu.np>
2026-08-07 06:37:51 +0545

    sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,16 +1,17 @@
 import { getBlogPosts } from 'app/creatives/utils'
 
+export const baseUrl = 'https://kendru.xyz'
 
 export default async function sitemap() {
-  let blogs = getBlogPosts().map((post) => ({
-    url: `${baseUrl}/blog/${post.slug}`,
+  const creatives = getBlogPosts().map((post) => ({
+    url: `${baseUrl}/creatives/${post.slug}`,
     lastModified: post.metadata.publishedAt,
   }))
 
-  let routes = ['', '/blog'].map((route) => ({
+  const routes = ['/projects', '/creatives', '/contact'].map((route) => ({
     url: `${baseUrl}${route}`,
-    lastModified: new Date().toISOString().split('T')[0],
+    lastModified: new Date().toISOString(),
   }))
 
-  return [...routes, ...blogs]
+  return [...routes, ...creatives]
 }
