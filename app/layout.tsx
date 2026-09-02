9ad8e6736a876bf7cbff2073ad17f4afa61ce2a2
iamkendru <078bei014@ioepc.edu.np>
2026-09-02 08:23:35 +0545

    favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,6 +15,9 @@ export const metadata: Metadata = {
     template: '%s | Next.js Portfolio Starter',
   },
   description: 'This is my portfolio.',
+  icons: {
+    icon: '/favicon.ico',
+  },
   openGraph: {
     title: 'My Portfolio',
     description: 'This is my portfolio.',
