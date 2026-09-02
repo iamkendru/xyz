0f3a3336e0978e3337b1e8c9ddcafed6b0dcbe33
iamkendru <078bei014@ioepc.edu.np>
2026-09-02 08:05:36 +0545

    update on home

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+import Link from 'next/link'
+
 export default function Page() {
   return (
     <section>
@@ -5,10 +7,10 @@ export default function Page() {
         Kendra Pokhrel (kendru)
       </h1>
       <p className="mb-4">
-        {`I'm Kendra Pokhrel, a technology enthusiast and an Electronics and Communication Engineering enthusiast.
+        {`I'm Kendra Pokhrel, a technology and a computer enthusiast. 
         I've been learning web development components such as react, typescript and most part of frontend development.
         I also have experience in hardware, IoT, networking and partly homelabbing, as well as I have spent my fair share of time modifying embedded hardwares.
-        I am a self made photographer and also have experience in literature.`}
+        I am a self made <Link href="https://instagram.com/kendrukophotography">photographer</Link> and also have experience in literature.`}
       </p>
     </section>
   )
